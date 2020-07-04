0033e887421f911d58eb00bdd235d8471bbbe08d
skyzen <piotr.ultimo@gmail.com>
2020-07-04 20:14:04 +0200

    Date and time: 20:14:03, 07/04/2020

--- AutoGreen.py
+++ AutoGreen.py
@@ -12,11 +12,24 @@ def do_changes(file):
     with open(file, "a") as f:
         f.write(f"Date and time: {datetime.now().strftime('%H:%M:%S, %m/%d/%Y')}\n")
 
+
+def get_status(repo, path):
+    changed = [ item.a_path for item in repo.index.diff(None) ]
+    if path in repo.untracked_files:
+        return 'Untracked'
+    elif path in changed:
+        return 'Modified'
+    else:
+        return 'Doesen\'t matter'
+
+print(get_status(repository, working_tree_dir))
+
 def git_go_green(repo):
-    if len(repo.untracked_files):
-        repo.git.add(A=True)
-        repo.git.commit('-m', f"Date and time: {datetime.now().strftime('%H:%M:%S, %m/%d/%Y')}")
-        repo.git.push('origin', 'HEAD:refs/for/master')
+    repo.git.add(A=True)
+    repo.git.commit('-m', f"Date and time: {datetime.now().strftime('%H:%M:%S, %m/%d/%Y')}")
+    repo.git.push('origin', 'HEAD:Git-Greener')
+    print('Pushed')
+
 
 do_changes(stampler)
 git_go_green(repository)
